b0ed1d9f3a4feaa4dc3e09182fb02edefcece3a9
joystream20 <fujita@joystream.co.jp>
2022-09-28 21:40:41 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+</script>
+
+<template>
+  <GuestLayout>
+    <Label value="件名　">タイトル</Label>
+    <Input modelValue="default words"></Input>
+  </GuestLayout>
+</template>
