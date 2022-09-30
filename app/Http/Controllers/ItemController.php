dac5669f113c174a1f1421e450b3f345b774232c
joystream20 <fujita@joystream.co.jp>
2022-09-30 22:17:36 +0900

    商品関連

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreitemRequest;
 use App\Http\Requests\UpdateitemRequest;
 use App\Models\item;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
@@ -15,7 +16,11 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        // $items = Item::select('id', 'name', 'price', 'is_selling')->get();
+        return Inertia::render('Items/Index', 
+        [
+          'items' => Item::select('id', 'name', 'price', 'is_selling')->get()
+        ]);
     }
 
     /**
@@ -25,7 +30,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
@@ -36,7 +41,17 @@ class ItemController extends Controller
      */
     public function store(StoreitemRequest $request)
     {
-        //
+      Item::create([
+        'name' => $request->name,
+        'memo' => $request->memo,
+        'price' => $request->price
+      ]);
+
+      return to_route('items.index')
+      ->with([
+        'message'=>'登録しました。',
+        'status'=>'success'
+      ]);
     }
 
     /**
@@ -47,7 +62,9 @@ class ItemController extends Controller
      */
     public function show(item $item)
     {
-        //
+        return Inertia::render('Items/Show', [
+          'item' => $item
+        ]);
     }
 
     /**
@@ -58,7 +75,9 @@ class ItemController extends Controller
      */
     public function edit(item $item)
     {
-        //
+        return Inertia::render('Items/Edit', [
+          'item' => $item
+        ]);
     }
 
     /**
@@ -70,7 +89,17 @@ class ItemController extends Controller
      */
     public function update(UpdateitemRequest $request, item $item)
     {
-        //
+        $item->name = $request->name;
+        $item->memo = $request->memo;
+        $item->price = $request->price;
+        $item->is_selling = $request->is_selling;
+        $item->save();
+
+        return to_route('items.index')
+        ->with([
+          'message' => 'updated!',
+          'status' => 'success'
+        ]);
     }
 
     /**
@@ -81,6 +110,12 @@ class ItemController extends Controller
      */
     public function destroy(item $item)
     {
-        //
+        $item->delete();
+
+        return to_route('items.index')
+        ->with([
+          'message' => 'deleted!',
+          'status' => 'danger'
+        ]);
     }
 }
---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+  <div v-if="$page.props.flash.status === 'success'" class="bg-blue-500 text-white px-4 py-4 mb-4">{{ $page.props.flash.message }}</div>
+  <div v-if="$page.props.flash.status === 'danger'" class="bg-red-500 text-white px-4 py-4 mb-4">{{ $page.props.flash.message }}</div>
+</template>
